0519e12bf74e56ac54442fcd4838881703a830f2
Johan <sejongbgy07@gmail.com>
2022-10-25 18:50:03 +0900

    avatar"

---
+++ src/AppJSX.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function AppJSX() {
+  return (
+    <>
+      <h1>Hello!</h1>
+      <h2>world</h2>
+      <ul>
+        <li></li>
+      </ul>
+    </>
+  );
+}
+
+export default AppJSX;
